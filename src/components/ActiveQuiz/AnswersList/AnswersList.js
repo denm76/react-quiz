0fd010a64cd37e696d87227432cfae7abc2177ed
denm76 <denm76@gmail.com>
2021-10-24 12:49:36 +0300

    Добавлена логика проверки правильности ответа и изменение цвета поля вопроса в соответствии с правильностью ответа

--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -2,19 +2,20 @@ import React from 'react'
 import AnswerItem from "./AnswerItem/AnswerItem";
 import classes from './AnswersList.module.css'
 
-const AnswersList = props =>(
+const AnswersList = props => (
     <ul className={classes.AnswersList}>
-        {props.answers.map((answer, index)=>{
-           return (
-               <AnswerItem
-                key = {index}
-                answer = {answer}
-                onAnswerClick = {props.onAnswerClick}
-               />
-           )
+        {props.answers.map((answer, index) => {
+            return (
+                <AnswerItem
+                    key={index}
+                    answer={answer}
+                    onAnswerClick={props.onAnswerClick}
+                    state={props.state ? props.state[answer.id] : null}
+                />
+            )
         })}
     </ul>
-
 )
+
 
 export default AnswersList
--- src/containers/quiz/Quiz.js
+++ src/containers/quiz/Quiz.js
@@ -5,6 +5,7 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 class Quiz extends Component{
     state = {
         activeQuestion:0,
+        answerState: null,
         quiz:[
             {
                 question:'Какого цвета небо',
@@ -34,10 +35,42 @@ class Quiz extends Component{
     }
 
     onAnswerClickHandler = (answerId) =>{
-        console.log('Answer Id:', answerId);
-        this.setState({
-            activeQuestion:this.state.activeQuestion + 1
-        })
+        if(this.state.answerState){
+            const key = Object.keys(this.state.answerState)[0];
+            if(this.state.answerState[key] === 'success'){
+                return
+            }
+        }
+        const question = this.state.quiz[this.state.activeQuestion]; //определяем текущий вопрос
+
+        //Если ответ правильный, переключаем на следующий вопрос
+        if(question.rightAnswerId == answerId ){
+            this.setState({
+                answerState:{[answerId]:'success'}
+            })
+            const timeout = window.setTimeout(()=>{
+                //Если опрос закончен
+                if(this.isQuizFinished()){
+                    console.log('Finished!');
+                }else{
+                    this.setState({
+                        activeQuestion:this.state.activeQuestion + 1,
+                        answerState:null
+                    })
+                }
+            },2000)
+
+        }else{
+            this.setState({
+                answerState:{[answerId]:'error'},
+            })
+        }
+
+
+    }
+
+    isQuizFinished(){
+        return this.state.activeQuestion + 1 === this.state.quiz.length;
     }
 
     render() {
@@ -52,6 +85,7 @@ class Quiz extends Component{
                         onAnswerClick = {this.onAnswerClickHandler}
                         quizLength = {this.state.quiz.length}
                         answerNumber = {this.state.activeQuestion + 1}
+                        state = {this.state.answerState}
                     />
                 </div>
             </div>
